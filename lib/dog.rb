4e61c5d877bcdd3a429ad04cd41b31024201ad39
mischa talokonnikoff <mischatalokonnikoff@gmail.com>
2019-05-19 15:14:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,15 @@
 class Dog
-  def bark
-    puts "Woof!"
-end
-  def sit
-    puts "The Dog is sitting"
-  end
+
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
+
   def name
     @this_dogs_name
   end
-
 end
 
 lassie = Dog.new
-lassie.name ="Lassie"
+lassie.name = "Lassie"
+
 puts lassie.name
